70209cdfc94a9da021e0e705945fe39a0f82edf6
sofia <sofia.ceavdari@gmail.com>
2025-03-18 16:00:01 +0100

    eersrte commit

--- PrulariaAankoopData/Repositories/IActiecodesRepository.cs
+++ PrulariaAankoopData/Repositories/IActiecodesRepository.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrulariaAankoopData.Models;
 
 namespace PrulariaAankoopData.Repositories;
 public interface IActiecodesRepository
 {
+    Task<Actiecode> ToevoegActiecodeAsync(Actiecode actiecode);
+    bool IsActiCodeNieuw(string naam, DateTime geldigVanDatum, DateTime geldigTotDatum);
 }
--- PrulariaAankoopService/Services/ActiecodesService.cs
+++ PrulariaAankoopService/Services/ActiecodesService.cs
@@ -3,8 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PrulariaAankoopData.Models;
+using PrulariaAankoopData.Repositories;
 
 namespace PrulariaAankoopService.Services;
 public class ActiecodesService
 {
+    private IActiecodesRepository _actiecodesRepository;
+    public ActiecodesService(IActiecodesRepository actiecodesRepository)
+    {
+            _actiecodesRepository = actiecodesRepository;
+    }
+
+    public bool IsActiCodeNieuw(string naam, DateTime geldigVanDatum, DateTime geldigTotDatum) 
+    {
+        return _actiecodesRepository.IsActiCodeNieuw(naam, geldigVanDatum, geldigTotDatum);
+    }
+
+    public async Task RegistrerenActiecodeAsync(Actiecode actiecode) 
+    {
+        await _actiecodesRepository.ToevoegActiecodeAsync(actiecode);
+    }
 }
