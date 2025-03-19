9f5f07e809b1c3fb4e1f859497edf38261027613
Amaury Scheerens <amaury.scheerens@vdabcampus.be>
2025-03-19 15:51:13 +0100

    builders toegevoegd

--- PrulariaAankoopUI/Program.cs
+++ PrulariaAankoopUI/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
 using PrulariaAankoopData.Repositories;
+using PrulariaAankoopService.Services;
 using PrulariaAankoopUI;
 using System.Globalization;
 
@@ -10,8 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PrulariaComContext>(
         options => options.UseMySQL(
          builder.Configuration.GetConnectionString("PrulariaComConnection"),
-                           x => x.MigrationsAssembly("naamvanhetdataproject")));
+                           x => x.MigrationsAssembly("PrulariaAankoopData")));
 
+builder.Services.AddScoped<ArtikelenService>();
 builder.Services.AddControllersWithViews();
 
 builder.Services.Configure<RequestLocalizationOptions>(options =>
@@ -31,6 +33,16 @@ builder.Services.AddControllersWithViews((options) =>
     options.ModelBinderProviders.Insert(0, new CustomBinderProvider());
 });
 
+builder.Services.AddTransient<ActiecodesService>();
+builder.Services.AddTransient<IActiecodesRepository, SQLActiecodesRepository>();
+builder.Services.AddTransient<ArtikelenService>();
+builder.Services.AddTransient<IArtikelenRepository, SQLArtikelenRepository>();
+builder.Services.AddTransient<CategorieenService>();
+builder.Services.AddTransient<ICategorieenRepository, SQLCategorieenRepository>();
+builder.Services.AddTransient<LeveranciersService>();
+builder.Services.AddTransient<ILeveranciersRepository, SQLLeveranciersRepository>();
+builder.Services.AddTransient<SecurityService>();
+builder.Services.AddTransient<ISecurityRepository, SQLSecurityRepository>();
 
 var app = builder.Build();
 
