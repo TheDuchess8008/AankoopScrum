c79d05f4361974f8c77fa4e0136640fcbcd4e11b
Koen Smits <koen.smits@vdabcampus.be>
2025-03-24 10:33:38 +0100

    ViewModels toegevoegd al

---
+++ PrulariaAankoopUI/Models/CategorieViewModel.cs
@@ -0,0 +1,20 @@
+﻿using PrulariaAankoopData.Models;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrulariaAankoopUI.Models;
+
+public class CategorieViewModel
+{
+    public int CategorieId { get; set; }
+    [Display(Name = "Naam Van Categorie")]
+    public string Naam { get; set; } = null!;
+
+    public int? HoofdCategorieId { get; set; }
+    [Display(Name = "Naam Van Hoofd Categorie")]
+
+    public virtual Categorie? HoofdCategorie { get; set; }
+
+    public virtual ICollection<Categorie> Subcategorieën { get; set; } = new List<Categorie>();
+
+    public virtual ICollection<Artikel> Artikelen { get; set; } = new List<Artikel>();
+}
