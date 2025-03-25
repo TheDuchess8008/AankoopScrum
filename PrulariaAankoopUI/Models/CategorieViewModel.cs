a1facf723b670abdd02407f07f15768b3fa76533
Amaury Scheerens <amaury.scheerens@vdabcampus.be>
2025-03-25 12:01:53 +0100

    Displays aangepast voor betere naamgeving.

--- PrulariaAankoopUI/Models/CategorieViewModel.cs
+++ PrulariaAankoopUI/Models/CategorieViewModel.cs
@@ -6,11 +6,11 @@ namespace PrulariaAankoopUI.Models;
 public class CategorieViewModel
 {
     public int CategorieId { get; set; }
-    [Display(Name = "Naam Van Categorie")]
+    [Display(Name = "Naam")]
     public string Naam { get; set; } = null!;
 
     public int? HoofdCategorieId { get; set; }
-    [Display(Name = "Naam Van Hoofd Categorie")]
+    [Display(Name = "Hoofdcategorie")]
 
     public virtual Categorie? HoofdCategorie { get; set; }
 
