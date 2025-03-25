1c6dfd39930a3d51f1ea0556878fe7cd14d8cedb
Lesley Huygens <lesley.huygens@vdabcampus.be>
2025-03-25 11:50:25 +0100

    klein foutje opgelost

--- PrulariaAankoopUI/Models/ArtikelCategorieViewModel.cs
+++ PrulariaAankoopUI/Models/ArtikelCategorieViewModel.cs
@@ -5,8 +5,6 @@ public class ArtikelCategorieViewModel
 {
     public int ArtikelId { get; set; }
     public string? ArtikelNaam { get; set; }
-    [Required(ErrorMessage = "Selecteer een categorie.")]
     public int CategorieId { get; set; }
-    [Required(ErrorMessage = "Selecteer een categorie.")]
     public string? CategorieNaam { get; set; }
 }
