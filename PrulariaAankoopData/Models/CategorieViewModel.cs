da442c0aa70db9cd22b2f3c449abb6eb1d52018e
Amaury Scheerens <amaury.scheerens@vdabcampus.be>
2025-03-26 13:07:39 +0100

    ViewModels verplaatst naar datalaag

---
+++ PrulariaAankoopData/Models/CategorieViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrulariaAankoopData.Models;
+
+public class CategorieViewModel
+{
+    public int CategorieId { get; set; }
+    [Display(Name = "Naam")]
+    public string Naam { get; set; } = null!;
+
+    public int? HoofdCategorieId { get; set; }
+    [Display(Name = "Hoofdcategorie")]
+
+    public virtual Categorie? HoofdCategorie { get; set; }
+
+    public virtual ICollection<Categorie> Subcategorieën { get; set; } = new List<Categorie>();
+
+    public virtual ICollection<Artikel> Artikelen { get; set; } = new List<Artikel>();
+}
